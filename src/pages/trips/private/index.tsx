99ba30d8795218980d083075ac353a73c80502d6
skillzTheProgrammer <47523568+skillzTheProgrammer@users.noreply.github.com>
2023-08-06 07:11:25 +0100

    update trips routea

---
+++ src/pages/trips/private/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PrivateTrip() {
+  return (
+    <div>PrivateTrip</div>
+  )
+}
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,12 +1,22 @@
-import NotFound from "../pages/notFound";
-import Trips from "../pages/trips";
-import { TRIPS_URL } from "./frontend";
+import NotFound from "pages/notFound";
+import Trips from "pages/trips";
+import PrivateTrip from "pages/trips/private";
+import { PRIVATE_TRIPS_URL, TRIPS_URL } from "./frontend";
 
 export const mainRoutes = [
   {
     id: 1,
     path: TRIPS_URL,
     Component: Trips,
+    exact: true,
+    isProtected: false,
+    hide: false,
+    showBottomNav: false,
+  },
+  {
+    id: 1,
+    path: PRIVATE_TRIPS_URL,
+    Component: PrivateTrip,
     exact: true,
     isProtected: false,
     hide: false,
