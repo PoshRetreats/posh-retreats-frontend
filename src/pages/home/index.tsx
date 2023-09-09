30bb225c8092ee446556ff7cdc1f24c27518f7d7
skillzTheProgrammer <47523568+skillzTheProgrammer@users.noreply.github.com>
2023-09-09 12:52:15 +0100

    Update index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,14 @@
 import { HOME_HEADER } from "assets";
+import FAQ from "components/faq";
+import HowWeWork from "components/howWeWork";
 import MenuHeader from "components/menuHeader";
 
 export default function Home() {
 	return (
 		<>
 			<MenuHeader img={HOME_HEADER} />
+      <HowWeWork />
+      <FAQ />
 		</>
 	);
 }
