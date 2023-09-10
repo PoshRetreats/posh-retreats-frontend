112f740e31d5eb1ec82b6441c3491b8301fca51f
skillzTheProgrammer <47523568+skillzTheProgrammer@users.noreply.github.com>
2023-09-10 16:06:23 +0100

    add trip home

--- src/pages/admin/home/index.tsx
+++ src/pages/admin/home/index.tsx
@@ -1,17 +1,69 @@
 import AdminMenu from "components/menuHeader/admin";
-import { AdminContainer, AdminHomeContainer } from "./style";
+import {
+	AdminContainer,
+	AdminHomeContainer,
+	AdminHomeFlexDiv,
+	AllTripsCardContainer,
+	GreyText,
+	TripCardContainer,
+	TripCardList,
+  TripHeadText,
+} from "./style";
 import AdminHeaderTitle from "components/menuHeader/admin/HeaderTitle";
 
-import React from 'react'
+type BasicTripData = {
+	title: string;
+	total: string;
+	date: string;
+};
+const tripData: BasicTripData[] = [
+	{
+		title: "Explore Canary Island",
+		total: "24 registrations",
+		date: "05/05/2023 6:45 am",
+	},
+	{
+		title: "Explore Canary Island",
+		total: "24 registrations",
+		date: "05/05/2023 6:45 am",
+	},
+	{
+		title: "Explore Canary Island",
+		total: "24 registrations",
+		date: "05/05/2023 6:45 am",
+	},
+	{
+		title: "Explore Canary Island",
+		total: "24 registrations",
+		date: "05/05/2023 6:45 am",
+	},
+];
 
-export function AllTripsCard() {
-  return (
-    <div>
-      <h1>All Trips</h1>
-    </div>
-  )
+export function TripCard({ title, total, date }: BasicTripData) {
+	return (
+		<TripCardContainer>
+			<div>
+				<TripHeadText>{title}</TripHeadText>
+				<GreyText>{total}</GreyText>
+			</div>
+			<GreyText>{date}</GreyText>
+		</TripCardContainer>
+	);
 }
 
+export function AllTripsCard() {
+	return (
+		<AllTripsCardContainer>
+			<h1>All Trips</h1>
+			<GreyText>All Registrations</GreyText>
+			<TripCardList>
+				{tripData.map(({ title, total, date }: BasicTripData) => (
+					<TripCard title={title} total={total} date={date} />
+				))}
+			</TripCardList>
+		</AllTripsCardContainer>
+	);
+}
 
 export default function AdminHome() {
 	return (
@@ -19,6 +71,9 @@ export default function AdminHome() {
 			<AdminMenu />
 			<AdminHomeContainer>
 				<AdminHeaderTitle />
+				<AdminHomeFlexDiv>
+					<AllTripsCard />
+				</AdminHomeFlexDiv>
 			</AdminHomeContainer>
 		</AdminContainer>
 	);
--- src/pages/admin/home/style.ts
+++ src/pages/admin/home/style.ts
@@ -4,6 +4,7 @@ import media from "utilities/styles/media";
 export const AdminContainer = styled.div`
 	width: 100vw;
 	height: 100vh;
+	overflow-x: hidden;
 	padding-left: 300px;
 	background-color: #f9f9f9;
 
@@ -14,6 +15,77 @@ export const AdminContainer = styled.div`
 
 export const AdminHomeContainer = styled.div`
 	padding: 64px 40px 0 40px;
+	height: 100%;
+	${media.tablet`
+   
+	`}
+`;
+
+export const AdminHomeFlexDiv = styled.div`
+	display: flex;
+	height: 100%;
+	${media.tablet`
+   
+	`}
+`;
+
+export const AllTripsCardContainer = styled.div`
+	width: 50%;
+	height: 100%;
+	border-radius: 16px;
+	padding: 20px;
+	margin-top: 30px;
+	background: var(--White, #fff);
+	box-shadow: 0px 2px 4px -2px rgba(247, 164, 157, 0.06),
+		0px 4px 8px -2px rgba(247, 164, 157, 0.1);
+
+	h1 {
+		font-size: 24px;
+		font-style: normal;
+		font-weight: 600;
+		line-height: normal;
+		margin-bottom: 4px;
+	}
+	${media.tablet`
+   
+	`}
+`;
+
+export const TripCardList = styled.div`
+	${media.tablet`
+   
+	`}
+`;
+
+export const TripCardContainer = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-top: 24px;
+	${media.tablet`
+   
+	`}
+`;
+
+export const GreyText = styled.p`
+	color: var(--Grey-color, #c4c4c4);
+	font-size: 16px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: normal;
+	${media.tablet`
+   
+	`}
+`;
+
+export const TripHeadText = styled.p`
+	color: var(--Text-Color, #020e07);
+	font-size: 18px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: normal;
+  margin-bottom: 12px;
 	${media.tablet`
    
 	`}
