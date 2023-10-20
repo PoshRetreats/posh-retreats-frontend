f722fe7fbaa14a58cba274ff874d307e824e4693
skillzTheProgrammer <47523568+skillzTheProgrammer@users.noreply.github.com>
2023-10-20 05:28:48 +0100

    Update ProtectedRoute.tsx

--- src/utilities/ProtectedRoute.tsx
+++ src/utilities/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 // import { useLocation } from "react-router-dom";
 // import { getAuthCookie } from "./helpers";
-// import { useAuthStore } from "zustandStore/auth";
+
+import { ADMIN_LOGIN } from "routes/frontend";
 
 // function getAuthCookie() {
 //   return null;
@@ -13,16 +14,14 @@
 // const cookieToken = getAuthCookie();
 
 export default function ProtectedRoute({ children, isProtected }: any) {
-  // const authToken = useAuthStore((state: any) => state.token);
-  // const token = authToken || cookieToken;
-  // const location = useLocation();
+  const token = cookieToken;
+  const location = useLocation();
 
-  // if (isProtected && !token) {
-  //   return <Navigate to={WELCOME_URL} state={{ from: location }} replace />;
-  // } else if (!!token && !isProtected) {
-  //   return <Navigate to={HOME_URL} state={{ from: location }} replace />;
-  // } else {
-  //   return children;
-  // }
-  return children;
+  if (isProtected && !token) {
+    return <Navigate to={ADMIN_LOGIN} state={{ from: location }} replace />;
+  } else if (!!token && !isProtected) {
+    return <Navigate to={HOME_URL} state={{ from: location }} replace />;
+  } else {
+    return children;
+  }
 }
