209b352a6e367fba8bf556ccd9d712af8ad8a868
timiebi <kosutimiebinicholas@gmail.com>
2023-10-21 08:52:07 +0100

    addded button

--- src/pages/admin/trips/tripFeatures/index.tsx
+++ src/pages/admin/trips/tripFeatures/index.tsx
@@ -2,28 +2,57 @@ import { MuiInnputField } from "components/muiInputFields";
 import { CreateTripCardList, GreyText } from "../style";
 import { AddButton } from "components/buttons/addButton";
 import { TripProps } from "../types";
+import { Active } from "../tripDetails/style";
+import { ACTIVE_ICON } from "assets";
 
 // FEATURES
-export function TripFeatures({ groupTripDetails, setGroupTripDetails }: TripProps) {
+export function TripFeatures({ groupTripDetails, setGroupTripDetails, addedFeatures, setAddedFeatures }: TripProps | any) {
+
+	const addBreakdown = () => {
+		if (!addedFeatures.breakDown) {
+			// If addedFeatures.breakDown is not defined, initialize it as an empty array
+			addedFeatures.breakDown = [];
+		}
+
+		if (!addedFeatures.breakDown.includes(groupTripDetails.breakDown)) {
+			// Add the value to addedFeatures.breakDown
+			addedFeatures.breakDown.push(groupTripDetails.breakDown);
+			setAddedFeatures({ ...addedFeatures });
+		}
+
+		// Reset the input field
+		setGroupTripDetails({ ...groupTripDetails, breakDown: "" });
+	}
+
+
+	// console.log(addedFeatures.breakDown);
+
 	return (
 		<>
 			<div>
 				<h3>Features</h3>
 				<GreyText>What are the rules for this trip?</GreyText>
-
 				<CreateTripCardList>
 					<MuiInnputField
 						onchange={(e) =>
 							setGroupTripDetails({
 								...groupTripDetails,
-								breakthrough: e.target.value,
+								breakDown: e.target.value,
 							})
 						}
-						value={groupTripDetails.breakthrough}
+						value={groupTripDetails.breakDown}
 						type="text"
 						placeholder="Breakdown"
 					/>
-					<AddButton />
+					{addedFeatures.breakDown.length > 0 && addedFeatures.breakDown.map((items: any) =>
+					(
+						<div className="included_trips">
+							<section className="included_trips_condition">
+								<Active alt="active" src={ACTIVE_ICON} />
+								<span>{items}</span>
+							</section>
+						</div>))}
+					<AddButton onclick={addBreakdown} />
 					<MuiInnputField
 						onchange={(e) =>
 							setGroupTripDetails({ ...groupTripDetails, inclusion: e.target.value })
